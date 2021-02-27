dc7be55033e6f72a44b97d489cb8416b146eb4a8
Gustavo Davis <gustavo.davis@outlook.com>
2021-02-27 16:52:46 -0300

    Added context API to share state between components, added random challeges and calc to experience bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
 
 import styles from '../styles/pages/Home.module.css'
+import { ChallengeBox } from '../components/ChallengeBox'
 
 export default function Home() {
   return (
@@ -24,7 +25,9 @@ export default function Home() {
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
